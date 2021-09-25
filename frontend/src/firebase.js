fb4c6938ec234d3a4121178722d7ecc5249868e9
Sergey <titanicsv@yandex.ru>
2021-09-25 18:11:15 +0300

    initial

---
+++ frontend/src/firebase.js
@@ -0,0 +1,63 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import Filter from 'bad-words'
+import { ref, onUnmounted, computed } from 'vue'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyASdoY79gcrVOtqazDvi7HHX_EK1bTeYnE",
+    authDomain: "chat-levkovo.firebaseapp.com",
+    projectId: "chat-levkovo",
+    storageBucket: "chat-levkovo.appspot.com",
+    messagingSenderId: "226695678832",
+    appId: "1:226695678832:web:173f235245a232daf71db5",
+    measurementId: "G-P558HHGX3P"
+})
+
+const auth = firebase.auth()
+
+export function useAuth() {
+    const user = ref(null)
+    const unsubscribe = auth.onAuthStateChanged(_user => (user.value = _user))
+    onUnmounted(unsubscribe)
+    const isLogin = computed(() => user.value !== null)
+
+    const signIn = async () => {
+        const googleProvider = new firebase.auth.GoogleAuthProvider()
+        await auth.signInWithPopup(googleProvider)
+    }
+    const signOut = () => auth.signOut()
+
+    return { user, isLogin, signIn, signOut }
+}
+
+const firestore = firebase.firestore()
+const messagesCollection = firestore.collection('messages')
+const messagesQuery = messagesCollection.orderBy('createdAt', 'desc').limit(100)
+const filter = new Filter()
+
+export function useChat() {
+    const messages = ref([])
+    const unsubscribe = messagesQuery.onSnapshot(snapshot => {
+        messages.value = snapshot.docs
+            .map(doc => ({ id: doc.id, ...doc.data() }))
+            .reverse()
+    })
+    onUnmounted(unsubscribe)
+
+    const { user, isLogin } = useAuth()
+    const sendMessage = text => {
+        if (!isLogin.value) return
+        const { photoURL, uid, displayName } = user.value
+        messagesCollection.add({
+            userName: displayName,
+            userId: uid,
+            userPhotoURL: photoURL,
+            text: filter.clean(text),
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+    }
+
+    return { messages, sendMessage }
+}
